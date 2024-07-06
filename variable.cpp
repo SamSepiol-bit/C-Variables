d8f45711a0b61ecdc33a0ca41c468d94cc26515a
SamSepiol-bit <icbtworkfor@gmail.com>
2024-07-06 19:52:49 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string StudentName = "Gimhana";
+	int StudentId = 76 ;
+	double Weight = 34.76;
+	char MyChar = 'A';
+	bool MyBool = true;
+	bool MyBool2 = false;
+
+	cout << "My Name is " << StudentName << endl;
+	cout << " ID Number is " << StudentId << endl;
+	cout << " Student Weight is" << Weight << endl;
+	cout << " Maths marks= " << MyChar << endl;
+	cout << " Boolean value is : " << MyBool << endl;
+	cout << " Boolean value is : " << MyBool2 << endl;
+}
